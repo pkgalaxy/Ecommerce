059b0e07e0c7f352e01286eb04f18ff8ea294a3f
pkgalaxy <pkgalaxyworld@gmail.com>
2023-09-15 15:12:19 +0530

    third commit

--- amazon/settings.py
+++ amazon/settings.py
@@ -123,18 +123,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = 'my-media/'
+MEDIA_URL = 'media/'
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 import os
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static'),]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
